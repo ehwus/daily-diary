b6fae2e33d7dc059cbf0a29c4e8e89bd257c58d4
Alex Withington-Smith <alex@awithsmith.com>
2020-11-18 09:22:05 +0000

    updated db information

--- db/create_diary_table.sql
+++ db/create_diary_table.sql
@@ -1,3 +1,3 @@
 -- Create 'diary_manager' and 'diary_manager_test' databases
 -- run the command below in each
-CREATE TABLE entries(id SERIAL PRIMARY KEY, entry TEXT);
+CREATE TABLE entries(id SERIAL PRIMARY KEY, title VARCHAR(30), body TEXT);
