1388837b2e6098876b65aafd0a119a5e560ae8e6
Alex Withington-Smith <alex@awithsmith.com>
2020-11-19 11:05:59 +0000

    added .create and .all class methods to Diary

--- lib/diary.rb
+++ lib/diary.rb
@@ -1,11 +1,32 @@
-require 'database_connection'
+require 'pg'
 
 class Diary
-  def initialize(database_class = DatabaseConnection)
-    @database = database_class
+  # def add(title, body)
+  #   @database.query("INSERT INTO entries (id, title, body) VALUES (DEFAULT, '#{title}', '#{body}');")
+  # end
+  def self.create(title:, body:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'diary_manager_test')
+    else
+      connection = PG.connect(dbname: 'diary_manager')
+    end
+    connection.exec("INSERT INTO entries (title, body) VALUES ('#{title}', '#{body}');")
   end
 
-  def add(title, body)
-    @database.query("INSERT INTO entries (id, title, body) VALUES (DEFAULT, '#{title}', '#{body}');")
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'diary_manager_test')
+    else
+      connection = PG.connect(dbname: 'diary_manager')
+    end
+    selection = connection.exec("SELECT * FROM entries;")
+    selection.map { |entry| Diary.new(title: entry['title'], body: entry['body']) }
+  end
+  
+  attr_reader :title, :body
+
+  def initialize(title:, body:)
+    @title = title
+    @body = body
   end
 end
--- spec/unit/diary_spec.rb
+++ spec/unit/diary_spec.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 require 'diary'
+require 'pg'
 
 describe Diary do
-  let(:database_class) { double :database_class }
-  let(:test_diary) { Diary.new(database_class) }
-  describe '#add' do
-    it 'stores a diary entry in the database with title and body' do
-      expect(database_class).to receive(:query).with("INSERT INTO entries (id, title, body) VALUES (DEFAULT, 'Dear Diary', 'Test Body');")
-      test_diary.add("Dear Diary", "Test Body")
+  it "initialises with a title and body" do
+    test = Diary.new(title: "Test", body: "Test Body")
+    expect(test.title).to eq("Test")
+    expect(test.body).to eq("Test Body")
+  end
+
+  describe ".create, .all" do
+    it "Creates a new entry with a body and title, retrievable in array with .all" do
+      Diary.create(title: "Foo", body: "Bar")
+      entry = Diary.all.first
+      expect(entry).to be_instance_of(Diary)
+      expect(entry.title).to eq("Foo")
+      expect(entry.body).to eq("Bar")
     end
   end
 end
